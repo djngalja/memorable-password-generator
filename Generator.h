733f8b10105cdd40bedcc87d47e77f539d95b52f
Galina <djngalja@gmail.com>
2025-04-06 21:38:19 +0300

    Update Generator.h

--- Generator.h
+++ Generator.h
@@ -13,30 +13,8 @@
 
 class Generator {
     std::vector<std::string> m_input_vector;
-    std::vector<std::string> m_temp_vector;
     std::size_t m_length;
     const std::string m_special_chars = "'-!\"#$%&()*,./:;?@[]^_`{|}~+<=>";
-    void ResizeVector();
-    void CopyVector(std::vector<std::string> &temp_vector, std::size_t length);
-    bool ContainsDigits();
-    bool ContainsLetters();
-    bool ContainsUpperCase();
-    bool ContainsLowerCase();
-    bool ContainsLowerCase(const std::string &str);
-    bool ContainsChar(char c);
-    bool ContainsSpecialChar();
-    bool ContainsSpecialChar(const std::string &str);
-    void AddDigitsOrLetters(bool add_letters = false);
-    void AddLowerOrUpperCase(bool add_upper = false);
-    char RandomSpecialChar();
-    char RandomLowerCase();
-    char RandomUpperCase();
-    char RandomDigit();
-    char RandomChar();
-    char AddChar(const std::string &str, bool last = false);
-    std::string JoinVector();
-    void FinalCheck(std::string &str);
-    void ReplaceChar(std::string &str, char c);
     void Test();
 public:
     Generator();
@@ -46,9 +24,31 @@ public:
 };
 
 // helper functions:
+char add_char(const std::string&, const std::string&,
+              const std::vector<std::string>&, bool = false);
+void replace_char(std::string&, char);
+char rand_char(const std::string&);
+char rand_digit();
+char rand_up_case();
+char rand_low_case();
+char rand_sp_char(const std::string&);
+bool has_sp_char(const std::string&, const std::vector<std::string>&);
+bool has_sp_char(const std::string&, const std::string&);
+void final_check(const std::string&, std::string& str);
+std::string join_vec(const std::vector<std::string>&, std::size_t);
+void add_low_or_up_case(std::vector<std::string>&, bool = false);
+void add_digit_or_letters(std::vector<std::string>&, bool = false);
+void resize_vec(std::size_t, std::vector<std::string>&);
+void resize_vec_recur(const std::vector<std::string>&, std::size_t, std::vector<std::string>&);
 void shuffle_vec(std::vector<std::string>&);
+bool has_char(const std::vector<std::string>&, char);
+bool has_low_case(const std::vector<std::string>&);
+bool has_low_case(const std::string&);
+bool has_letter(const std::vector<std::string>&);
 bool has_digit(const std::string&);
+bool has_digit(const std::vector<std::string>&);
 bool has_up_case(const std::string&);
+bool has_up_case(const std::vector<std::string>&);
 bool get_questions(const std::string&, std::vector<std::string>&);
 std::vector<std::string> get_input();
 std::size_t get_len();
