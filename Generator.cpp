b9fd18cc8a980b6e20f2f15fe0ef2c9fcdc8a64c
Galina <djngalja@gmail.com>
2025-04-12 22:15:51 +0300

    Update Generator.cpp

--- Generator.cpp
+++ Generator.cpp
@@ -1,11 +1,11 @@
 #include "Generator.h"
 
-Generator::Generator() {
+pswd::Generator::Generator() {
     get_input();
     get_len();
 }
 
-void Generator::get_input() {
+void pswd::Generator::get_input() {
     std::vector<std::string> questions;
     if (!get_questions("security_questions.txt", questions)) {
         // TODO
@@ -19,26 +19,26 @@ void Generator::get_input() {
     }
 }
 
-void Generator::get_len() {
+void pswd::Generator::get_len() {
     std::cout << "Enter password length: ";
     std::cin >> m_len; // TODO: input check
 }
 
-void Generator::generate_password() {
+void pswd::Generator::generate_password() {
     std::vector<std::string> vec_copy {m_input_vec};
     shuffle_vec(vec_copy);
     resize_vec(m_len, vec_copy);
     if (!has_digit(vec_copy)) {
-            add_digit_or_letters(vec_copy);
+        add_digit(vec_copy);
     }
     if (!has_letter(vec_copy)) {
-            add_digit_or_letters(vec_copy, true);
+        add_letters(vec_copy);
     }
     if (!has_up_case(vec_copy)) {
-            add_low_or_up_case(vec_copy, true);
+        add_up_case(vec_copy);
     }
     if (!has_low_case(vec_copy)) {
-            add_low_or_up_case(vec_copy);
+        add_low_case(vec_copy);
     }
     std::string password = join_vec(m_special_chars, vec_copy, m_len);
     final_check(m_special_chars, password);
@@ -46,7 +46,7 @@ void Generator::generate_password() {
 }
 
 
-bool get_questions(const std::string& f_name, std::vector<std::string>& questions) {
+bool pswd::get_questions(const std::string& f_name, std::vector<std::string>& questions) {
     std::ifstream file(f_name);
     if (!file) {
         std::cout << "File <" << f_name << "> not found\n";
@@ -58,13 +58,13 @@ bool get_questions(const std::string& f_name, std::vector<std::string>& question
     return true;
 }
 
-void shuffle_vec(std::vector<std::string>& vec) {
+void pswd::shuffle_vec(std::vector<std::string>& vec) {
     static std::random_device rd;
     std::mt19937 g(rd());
     std::shuffle(vec.begin(), vec.end(), g);
 }
 
-void split_input(const std::string& str, std::vector<std::string>& vec) {
+void pswd::split_input(const std::string& str, std::vector<std::string>& vec) {
     std::istringstream line(str);
     for (std::string str2; getline(line, str2, ' ');) {
         if (!str2.empty()) {
@@ -73,13 +73,13 @@ void split_input(const std::string& str, std::vector<std::string>& vec) {
     }
 }
 
-void resize_vec(std::size_t len, std::vector<std::string>& vec) {
+void pswd::resize_vec(std::size_t len, std::vector<std::string>& vec) {
     std::vector<std::string> temp_vec;
     resize_vec_recur(vec, len, temp_vec);
     vec = temp_vec;
 }
 
-void resize_vec_recur(const std::vector<std::string>& vec, std::size_t len, std::vector<std::string>& temp_vec) {
+void pswd::resize_vec_recur(const std::vector<std::string>& vec, std::size_t len, std::vector<std::string>& temp_vec) {
     std::size_t new_len {};
     for (const std::string& str : vec) {
         new_len += str.size() + 1; // every word will be followed by a random character
@@ -100,25 +100,24 @@ void resize_vec_recur(const std::vector<std::string>& vec, std::size_t len, std:
     if (new_len < len) resize_vec_recur(vec, len - new_len, temp_vec);
 }
 
-bool has_digit(const std::string& str) {
+bool pswd::has_digit(const std::string& str) {
     for (char c : str) {
         if (std::isdigit(c)) { return true; }
     }
     return false;
 }
 
-bool has_digit(const std::vector<std::string>& vec) {
+bool pswd::has_digit(const std::vector<std::string>& vec) {
     for (const auto& str : vec) {
         if (has_digit(str)) { return true; }
     }
     return false;
 }
 
-void add_digit_or_letters(std::vector<std::string>& vec, bool add_letters) {
+void pswd::add_digit(std::vector<std::string>& vec) {
     std::vector<std::vector<char>> replacements = {
         {'b', '6'}, {'q', '9'}, {'S', '5'}, {'l', '1'}, {'O', '0'}
     };
-    // Add digits by default
     for (const auto& rep : replacements) {
         if (has_char(vec, rep[0])) {
             for (std::string& str : vec) {
@@ -130,29 +129,33 @@ void add_digit_or_letters(std::vector<std::string>& vec, bool add_letters) {
             break;
         }
     }
-    if (add_letters) {
-        for (const auto& rep : replacements) {
-            if (has_char(vec, rep[1])) {
-                for (std::string& str : vec) {
-                    for (char& c : str) {
-                        if (c == rep[1]) { c = rep[0]; }
-                    }
+}
+
+void pswd::add_letters(std::vector<std::string>& vec) {
+    std::vector<std::vector<char>> replacements = {
+        {'b', '6'}, {'q', '9'}, {'S', '5'}, {'l', '1'}, {'O', '0'}
+    };
+    for (const auto& rep : replacements) {
+        if (has_char(vec, rep[1])) {
+            for (std::string& str : vec) {
+                for (char& c : str) {
+                    if (c == rep[1]) { c = rep[0]; }
                 }
             }
         }
     }
 }
 
-bool has_char(const std::vector<std::string>& vec, char c) {
+bool pswd::has_char(const std::vector<std::string>& vec, char c) {
     for (const std::string& str : vec) {
-        for (char i : str) {
-            if (i == c) { return true; }
+        if (str.find(c) != std::string::npos) {
+            return true;
         }
     }
     return false;
 }
 
-bool has_letter(const std::vector<std::string>& vec) {
+bool pswd::has_letter(const std::vector<std::string>& vec) {
     for (const auto& str : vec) {
         for (char c : str) {
             if (isalpha(c)) { return true; }
@@ -161,53 +164,54 @@ bool has_letter(const std::vector<std::string>& vec) {
     return false;
 }
 
-bool has_up_case(const std::string& str) {
+bool pswd::has_up_case(const std::string& str) {
     for (char c : str) {
         if (std::isupper(c)) { return true; }
     }
     return false;
 }
 
-bool has_up_case(const std::vector<std::string>& vec) {
+bool pswd::has_up_case(const std::vector<std::string>& vec) {
     for (const auto& str : vec) {
         if (has_up_case(str)) { return true; }
     }
     return false;
 }
 
-void add_low_or_up_case(std::vector<std::string>& vec, bool add_up) {
+void pswd::add_up_case(std::vector<std::string>& vec) {
     static Rand rand_int {0, 99};
-    if (add_up) {
-        for (auto& str: vec) {
-            for (char& c: str) {
-                // probability is only 33% so that password is easier to memorise
-                if (rand_int() < 33) { c = toupper(c); }
-            }
-        }
-    } else {
-        for (auto& str: vec) {
-            for (char& c: str) {
-                if (rand_int() < 33) { c = tolower(c); }
-            }
+    for (auto& str: vec) {
+        for (char& c: str) {
+            // probability is only 33% so that password is easier to memorise
+            if (rand_int() < 33) { c = toupper(c); }
         }
     }
 }
 
-bool has_low_case(const std::string& str) {
+void pswd::add_low_case(std::vector<std::string>& vec) {
+    static Rand rand_int {0, 99};
+    for (auto& str: vec) {
+        for (char& c: str) {
+            if (rand_int() < 33) { c = tolower(c); }
+        }
+    }
+}
+
+bool pswd::has_low_case(const std::string& str) {
     for (char c : str) {
         if (islower(c)) { return true; }
     }
     return false;
 }
 
-bool has_low_case(const std::vector<std::string>& vec) {
+bool pswd::has_low_case(const std::vector<std::string>& vec) {
     for (const std::string& str : vec) {
         if (has_low_case(str)) { return true;}
     }
     return false;
 }
 
-std::string join_vec(const std::string& sp_chars, const std::vector<std::string>& vec, std::size_t len) {
+std::string pswd::join_vec(const std::string& sp_chars, const std::vector<std::string>& vec, std::size_t len) {
     std::string password;
     for (const std::string& str : vec) {
         password += str + add_char(sp_chars, password, vec);
@@ -218,25 +222,35 @@ std::string join_vec(const std::string& sp_chars, const std::vector<std::string>
     return password;
 }
 
-char add_char(const std::string& sp_chars, const std::string& str,
-              const std::vector<std::string>& vec, bool last) {
-    if (last) {
-        if (!has_sp_char(sp_chars, str)) return rand_sp_char(sp_chars);
-        else if (!has_digit(str)) return rand_digit();
-        else if (!has_up_case(str)) return rand_up_case();
-        else if (!has_low_case(str)) return rand_low_case();
-        else return rand_char(sp_chars);
+char pswd::add_char(const std::string& sp_chars, const std::string& str,
+              const std::vector<std::string>& vec, bool last_char) {
+    if (last_char) {
+        if (!has_sp_char(sp_chars, str)) {
+            return rand_sp_char(sp_chars);
+        }
+        if (!has_digit(str)) { return rand_digit(); }
+        if (!has_up_case(str)) { return rand_up_case(); }
+        if (!has_low_case(str)) { return rand_low_case(); }
+        return rand_char(sp_chars);
     }
     else {
-        if (!(has_sp_char(sp_chars, str) || has_sp_char(sp_chars, vec))) return rand_sp_char(sp_chars);
-        else if (!(has_digit(str) || has_digit(vec))) return rand_digit();
-        else if (!(has_up_case(str) || has_up_case(vec))) return rand_up_case();
-        else if (!(has_low_case(str) || has_low_case(vec))) return rand_low_case();
-        else return rand_char(sp_chars);
+        if (!(has_sp_char(sp_chars, str) || has_sp_char(sp_chars, vec))) {
+            return rand_sp_char(sp_chars);
+        }
+        if (!(has_digit(str) || has_digit(vec))) {
+            return rand_digit();
+        }
+        if (!(has_up_case(str) || has_up_case(vec))) {
+            return rand_up_case();
+        }
+        if (!(has_low_case(str) || has_low_case(vec))) {
+            return rand_low_case();
+        }
+        return rand_char(sp_chars);
     }
 }
 
-bool has_sp_char(const std::string& sp_chars, const std::string& str) {
+bool pswd::has_sp_char(const std::string& sp_chars, const std::string& str) {
     for (char c : sp_chars) {
         if (str.find(c) != std::string::npos) {
                 return true;
@@ -245,41 +259,40 @@ bool has_sp_char(const std::string& sp_chars, const std::string& str) {
     return false;
 }
 
-bool has_sp_char(const std::string& sp_chars, const std::vector<std::string>& vec) {
+bool pswd::has_sp_char(const std::string& sp_chars, const std::vector<std::string>& vec) {
     for (const std::string& str : vec) {
         if (has_sp_char(sp_chars, str)) { return true; }
     }
     return false;
 }
 
-char rand_sp_char(const std::string& sp_chars) {
+char pswd::rand_sp_char(const std::string& sp_chars) {
     int sz = sp_chars.size();
-    static Rand rand_int {0, sz};
+    static Rand rand_int {0, sz - 1};
     return sp_chars[rand_int()];
 }
 
-char rand_digit() {
+char pswd::rand_digit() {
     std::string digits = "0123456789";
     static Rand rand_int {0, 9};
     return digits[rand_int()];
 }
 
-char rand_up_case() {
+char pswd::rand_up_case() {
     std::string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     static Rand rand_int {0, 25};
     return letters[rand_int()];
 }
 
-char rand_low_case() {
+char pswd::rand_low_case() {
     std::string letters = "abcdefghijklmnopqrstuvwxyz";
     static Rand rand_int {0, 25};
     return letters[rand_int()];
 }
 
-char rand_char(const std::string& sp_chars) {
+char pswd::rand_char(const std::string& sp_chars) {
     static Rand rand_int {0, 3};
-    int c = rand_int();
-    switch (c) {
+    switch (rand_int()) {
         case 0: {
             return rand_digit();
         }
@@ -295,7 +308,7 @@ char rand_char(const std::string& sp_chars) {
     }
 }
 
-void final_check(const std::string& sp_chars, std::string& str) {
+void pswd::final_check(const std::string& sp_chars, std::string& str) {
     if (!has_digit(str)) {
         replace_char(str, rand_digit());
     }
@@ -310,7 +323,7 @@ void final_check(const std::string& sp_chars, std::string& str) {
     }
 }
 
-void replace_char(std::string& str, char c) {
+void pswd::replace_char(std::string& str, char c) {
     int digit_cnt {};
     int low_cnt {};
     int up_cnt {};
@@ -318,14 +331,11 @@ void replace_char(std::string& str, char c) {
     for (char& ch : str) {
         if (isdigit(ch)) {
             digit_cnt++;
-        }
-        else if (islower(ch)) {
+        } else if (islower(ch)) {
             low_cnt++;
-        }
-        else if (isupper(ch)) {
+        } else if (isupper(ch)) {
             up_cnt++;
-        }
-        else { sp_cnt++; }
+        } else { sp_cnt++; }
         if (digit_cnt > 1 || low_cnt > 1 || up_cnt > 1 || sp_cnt > 1) {
             // There is at least 1 more char of the same type --> can replace
             ch = c;
